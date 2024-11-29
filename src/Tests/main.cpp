1a2eb984c3c8ac3397852f6c8e4ce421111ae675
LittleCoinCoin <eliott.jacopin@laposte.net>
2024-11-30 00:44:02 +0900

    [Add] Testing ExeConsoleLoggerSink
    Major:
    - Since I added the "full test purpose" TestLoggerSink, i wasn't testing the runtime of the ExeConsoleLoggerSink anymore
    - Hence, I readded it and repreint the same message.
    - By the way it also gave me the occasion to test the `RemoveSink` function (yes, I had forgotten since the beginning)

--- src/Tests/main.cpp
+++ src/Tests/main.cpp
@@ -1,11 +1,12 @@
 ﻿
+#include "Logging/ExeConsoleLoggerSink.hpp"
 #include "Logging/Logger.hpp"
 #include "Tests/TestLoggerSink.hpp"
 
 int main()
 {
-	Logging::TestLoggerSink logSink;
-	Logging::Logger::AddSink(new Logging::TestLoggerSink());
+	Logging::TestLoggerSink* logSink = new Logging::TestLoggerSink();
+	Logging::Logger::AddSink(logSink);
 
 	LOG_TRACE("This is a trace message.");
 	LOG_TRACE("This is a trace message with an int: %u", 42);
@@ -15,6 +16,24 @@ int main()
 	LOG_ERROR("This is an error message with the hex format of %u: %x", 42, 42);
 	LOG_WARNING("This is a warning message.");
 	LOG_WARNING("This is a warning message with a string: %s", "forty two");
+
+	Logging::Logger::RemoveSink(0);
+	delete logSink;
+
+	Logging::ExeConsoleLoggerSink* consoleSink = new Logging::ExeConsoleLoggerSink();
+	Logging::Logger::AddSink(consoleSink);
+
+	LOG_TRACE("This is a trace message.");
+	LOG_TRACE("This is a trace message with an int: %u", 42);
+	LOG_DEBUG("This is a debug message.");
+	LOG_DEBUG("This is a debug message with a float: %f", 42.123f);
+	LOG_ERROR("This is an error message.");
+	LOG_ERROR("This is an error message with the hex format of %u: %x", 42, 42);
+	LOG_WARNING("This is a warning message.");
+	LOG_WARNING("This is a warning message with a string: %s", "forty two");
+
+	Logging::Logger::RemoveSink(0);
+	delete consoleSink;
 
 	return 0;
 }
