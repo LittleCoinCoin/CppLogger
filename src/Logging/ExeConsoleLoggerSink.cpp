8d0c801619d8e7a1431663a06c1aeb39a537550d
LittleCoinCoin <eliott.jacopin@laposte.net>
2024-11-30 00:55:07 +0900

    [Update - Minor] Output msg of ExeConsoleLoggerSink
    Major:
    - Updating the characters of the prefixes.

--- src/Logging/ExeConsoleLoggerSink.cpp
+++ src/Logging/ExeConsoleLoggerSink.cpp
@@ -28,23 +28,23 @@ void Logging::ExeConsoleLoggerSink::LogWarning(const char* _msg, const char* _ti
 void Logging::ExeConsoleLoggerSink::PrefixTrace(const char* _time, const char* _file,
 	const char* _function, const int _line) const noexcept
 {
-	std::cout << "[TRACE | '" << _time << "' | '" << _file << " | '" << _function << "-" << _line << "']\n\t";
+	std::cout << "[TRACE | '" << _time << "' | '" << _file << " | '" << _function << "-" << _line << "']" << std::endl;
 }
 
 void Logging::ExeConsoleLoggerSink::PrefixDebug(const char* _time, const char* _file,
 	const char* _function, const int _line) const noexcept
 {
-	std::cout << "[DEBUG | '" << _time << "' | '" << _file << " | '" << _function << "-" << _line << "']\n\t";
+	std::cout << "[DEBUG | '" << _time << "' | '" << _file << " | '" << _function << "-" << _line << "']" << std::endl;
 }
 
 void Logging::ExeConsoleLoggerSink::PrefixError(const char* _time, const char* _file,
 	const char* _function, const int _line) const noexcept
 {
-	std::cout << "[ERROR | '" << _time << "' | '" << _file << " | '" << _function << "-" << _line << "']\n\t";
+	std::cout << "[ERROR | '" << _time << "' | '" << _file << " | '" << _function << "-" << _line << "']" << std::endl;
 }
 
 void Logging::ExeConsoleLoggerSink::PrefixWarning(const char* _time, const char* _file,
 	const char* _function, const int _line) const noexcept
 {
-	std::cout << "[WARNING | '" << _time << "' | '" << _file << " | '" << _function << "-" << _line << "']\n\t";
+	std::cout << "[WARNING | '" << _time << "' | '" << _file << " | '" << _function << "-" << _line << "']" << std::endl;
 }
