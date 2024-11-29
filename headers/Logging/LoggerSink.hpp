45112b6d48b51a13e4907ee75a30e524ed85be35
LittleCoinCoin <eliott.jacopin@laposte.net>
2024-11-29 13:19:40 +0900

    [Update] Separate LoggerSink source in hpp/cpp
    Major:
    - I am going to add some more code to the base class LoggerSink and I would like to keep the source code organisation homogeneous between files.
    - Hence, I am separating the existing source for LoggerSink into hpp and cpp (the latter was added).
    - There is no new feature in this commit.

--- headers/Logging/LoggerSink.hpp
+++ headers/Logging/LoggerSink.hpp
@@ -26,12 +26,6 @@ namespace Logging
 		LOGGING_API LoggerSink() = default;
 
 		LOGGING_API void Log(LogLevel _lvl, const char* _msg,
-			const char* _time, const char* _file, const char* _function, const int _line) noexcept
-		{
-			if (loggingFunctions[_lvl])
-			{
-				(this->*loggingFunctions[_lvl])(_msg, _time, _file, _function, _line);
-			}
-		}
+			const char* _time, const char* _file, const char* _function, const int _line) noexcept;
 	};
 }
